10c24f0a6a925c3efde555a104645dd47e07e033
h-otter <h-otter@outlook.jp>
2017-01-09 02:12:07 +0900

    update algorithm of needle speed change

--- src/ViewClock.java
+++ src/ViewClock.java
@@ -21,38 +21,58 @@ class ViewClock implements Observer {
     
   }
 
-  private static final int defaultMaxChangeMilliSec = 20000;
+  private static final int defaultMaxChangeMilliSec = 15000;
   private int maxChangeMilliSec;
-  private static final int defaultMinChangeMilliSec = 5000;
+  private static final int defaultMinChangeMilliSec = 3000;
   private int minChangeMilliSec;
   private long lastChangedMillSec;
   private Random rng;
 
 /**
  * deside whether change speed or not
- * @param currentTime start proc time
  *
  * [+] algorithm example
- *  random < maxChangeMilliSec
+ *  random < max
  *  <--------------->
- * |-------------|--------------|
- * last          currentTime    last + maxChangeMilliSec
+ * |------|------|--------------|
+ * last   min    currentTime    last + max
  * -> in this case, not change clockwise speed
  * -> binomial distribution
+ *
+ * @param currentTime start proc time
  */
   public void clockwise(long currentTime) {
-    if (rng.nextInt(maxChangeMilliSec) + lastChangedMillSec < currentTime && currentTime - lastChangedMillSec > minChangeMilliSec) {
-      lastChangedMillSec = currentTime;
+    if (this.rng.nextInt(this.maxChangeMilliSec - this.minChangeMilliSec) + this.lastChangedMillSec + this.minChangeMilliSec < currentTime) {
+      if (this.debugMode) {
+        System.out.println("[+] changed speed of clockwise in " + (currentTime - this.lastChangedMillSec));
+      }
+      this.lastChangedMillSec = currentTime;
       changeClockwise();
+      recalcParams();
+    }
+  }
+
+  private final static double maxParam = 0.93;
+  private final static double minParam = 0.95;
+
+/**
+ * recalc maxChangeMilliSec and minChangeMilliSec
+ * a_n = a_n-1 * params
+ */
+  private void recalcParams(){
+    this.maxChangeMilliSec = (int)(this.maxChangeMilliSec * (this.maxParam + this.rng.nextDouble() % 0.05));
+    this.minChangeMilliSec = (int)(this.minChangeMilliSec * (this.minParam + this.rng.nextDouble() % 0.05));
+    if (this.maxChangeMilliSec < this.minChangeMilliSec + 1000){
+      this.maxChangeMilliSec = this.minChangeMilliSec + 1000;
+    }
+
+    if (this.debugMode) {
+      System.out.println("[+] recalced maxChangeMilliSec = " + this.maxChangeMilliSec + ", minChangeMilliSec = " + this.minChangeMilliSec);
     }
   }
 
   private static final float speedRange = 1;
   private float speed;
 
-  private void changeClockwise(){
-    if (debugMode) {
-      System.out.println("[+] changed speed of clockwise");
-    }
-  }
+  private void changeClockwise(){}
 }
