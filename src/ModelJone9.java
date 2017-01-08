44e39d7b5955d76bf9551779eb6a8daeca2268f7
h-otter <h-otter@outlook.jp>
2017-01-08 22:58:01 +0900

    almost developed fps mechanism

--- src/ModelJone9.java
+++ src/ModelJone9.java
@@ -1,14 +1,21 @@
-package Jone9;
+// package Jone9;
 
 import java.util.Observable;
 
 
 
 class ModelJone9 extends Observable {
+/**
+ * when DEBUG MODE, debugMode set 1 on ModelJone9(String)
+ */
   private int debugMode;
 
   public ModelJone9(){
     this.jumpStatus = 0;
+
+    defaultIdealSleep = (long)((1000 << 16) / fps);
+    this.fpsThread = new FPSThread(this);
+    this.fpsThread.start();
   }
 
   private ModelJone9(String debug){
@@ -27,10 +34,78 @@ class ModelJone9 extends Observable {
   }
 
 /**
- * run() method called on fps procs
+ * gameUpdate() method called on fps procs
+ * core
  */
-  private void run(){
+  public void gameUpdate(){
+    if (this.debugMode == 1){
+      System.out.println("[*] heartbeats FPS = " + fpsThread.getFPS());
+    }
+
     notifyObservers();
+  }
+
+  private FPSThread fpsThread;
+  private static final float fps = 30;
+  private long defaultIdealSleep;  // ms
+
+  public long getDefaultIdealSleep(){
+    return defaultIdealSleep;
+  }
+
+  class FPSThread extends Thread {
+    private ModelJone9 gameModel;
+
+    public FPSThread(ModelJone9 origin){
+      this.gameModel = origin;
+      this.countStart = 0;
+      this.countFrame = 0;
+    }
+
+    long startTime;
+
+    @Override
+    public void run() {
+      long finishedTime, sleepTime;
+      long sleptTime = System.currentTimeMillis() << 16;
+      long error = 0;
+
+      while (true) {
+        startTime = sleptTime;
+
+        gameModel.gameUpdate();
+
+        finishedTime = System.currentTimeMillis() << 16;
+        sleepTime = gameModel.getDefaultIdealSleep() - (finishedTime - startTime) - error;
+        try {
+          this.sleep(sleepTime >> 16);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+
+        sleptTime = System.currentTimeMillis() << 16;
+        error = sleptTime - finishedTime - sleepTime;
+      }
+    }
+
+    private int countFrame;
+    private long countStart;
+
+/**
+ * 頭が働いてないので後で
+ * @return float FPS
+ */
+    public float getFPS(){
+      if (this.countFrame > 30){
+        float fps = ((startTime - countStart) >> 16) / this.countFrame;
+        // System.out.println(startTime);
+        this.countStart = this.startTime;
+        this.countFrame = 0;
+        return fps;
+      }
+      this.countFrame++;
+      return 0;
+    }
   }
 
 /**
