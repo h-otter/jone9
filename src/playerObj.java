49cace76c97d03a93d1f218e6adf3ae615d22e2b
kden <shoki0417@gmail.com>
2017-01-26 02:03:22 +0900

    add obj file loader

---
+++ src/playerObj.java
@@ -0,0 +1,61 @@
+import java.awt.*;
+import javax.swing.*;
+import java.io.*;
+import com.sun.j3d.loaders.objectfile.ObjectFile;
+import com.sun.j3d.loaders.ParsingErrorException;
+import com.sun.j3d.loaders.IncorrectFormatException;
+import com.sun.j3d.loaders.Scene;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import javax.media.j3d.*;
+import javax.vecmath.*;
+import com.sun.j3d.utils.universe.*;
+import com.sun.j3d.utils.image.TextureLoader;
+import com.sun.j3d.utils.geometry.*;
+import java.awt.GraphicsConfiguration;
+
+/* 
+ * 
+ */
+public class playerObj {
+	private Canvas3D c3d;
+	private ObjectFile f;
+	private Scene s;
+	private Bounds b;
+	private BranchGroup rootObj;
+	private TransformGroup trans;
+	
+	public playerObj(String filename, int arg){
+        trans = new TransformGroup();
+		f = new ObjectFile(arg);
+		rootObj = new BranchGroup();
+		
+		try
+        {
+        	// 読んでシーンに貼り付け
+            s = f.load(filename);
+            System.out.println("model: "+filename+" loaded.");
+        }
+        catch(Exception e)
+        {
+        	e.printStackTrace();
+            System.exit(1);
+        }
+	}
+	
+	public void addBounds(Bounds bounds)
+	{
+		b = bounds;
+	}
+	
+	public Node getSceneGroup()
+	{
+		return s.getSceneGroup();
+	}
+	
+	public TransformGroup getTransformGroup()
+	{
+		return trans;
+	}
+	
+}
