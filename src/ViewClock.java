a9c355ae785d9d4a3a06145e18365ad22e3b384f
kden <shoki0417@gmail.com>
2017-01-26 02:01:59 +0900

    change model to original object

--- src/ViewClock.java
+++ src/ViewClock.java
@@ -1,5 +1,6 @@
 import java.util.Random;
 
+import com.sun.j3d.loaders.objectfile.ObjectFile;
 import com.sun.j3d.utils.geometry.ColorCube;
 
 import javax.media.j3d.BranchGroup;
@@ -19,10 +20,14 @@ class ViewClock implements ViewInterface {
     this.minChangeMilliSec = defaultMinChangeMilliSec;
     this.lastChangedMillSec = 0;
 
-    ColorCube testCube = new ColorCube(0.1f);
+    //ColorCube testCube = new ColorCube(0.1f);
+    //tg = new TransformGroup();
+    //tg.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+    //tg.addChild(testCube);
     tg = new TransformGroup();
-    tg.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
-    tg.addChild(testCube);
+    playerObj po = new playerObj("assets/arrow2_fix.obj", ObjectFile.RESIZE);
+	tg.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+	tg.addChild(po.getSceneGroup());
     parentGroup.addChild(tg);
 
     rotValue = 0;
